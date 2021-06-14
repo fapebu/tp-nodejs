e7adc9191a3ea7fdec73370e6de9e4ff5b2cbb6d
Fabricio <fabri_0077@hotmail.com>
2021-06-13 23:18:57 -0300

    agregado categoria

--- app.js
+++ app.js
@@ -176,6 +176,132 @@ app.put('/persona/:id', async (req, res) => {
 
 });
 
+/*
+----------------------
+|INGRESO DE CATEGORIA|
+----------------------
+*/
+app.post('/categoria', async (req, res) => {
+    try {
+        //validacion de datos
+        if (!req.body.nombre.toUpperCase().trim()) {
+            throw new Error('Faltan datos');
+        }
+
+        //validacion de ingreso categoria repetida
+
+        let query = 'SELECT * FROM categoria WHERE nombre = ?';
+        let respuesta = await qy(query, [req.body.nombre.toUpperCase()]);
+        if (respuesta.length > 0) {
+            throw new Error('Ese nombre de categoria ya existe');
+        }
+
+        query = 'INSERT INTO categoria (nombre) VALUE (?)'
+
+        respuesta = await qy(query, req.body.nombre.toUpperCase());
+        
+        query = 'SELECT * FROM categoria WHERE nombre = ?';
+        respuesta = await qy(query, [req.body.nombre.toUpperCase()]);
+
+        res.status(200).send({respuesta});
+    }
+    catch (e) {
+        console.error(e.message);
+        res.status(403).send({ 'Error inesperado': e.message });
+    }
+})
+
+
+/*
+-------------------
+|MOSTRAR CATEGORIA|
+-------------------
+*/
+
+
+app.get('/categoria', async (req, res) => {
+    try {
+        if(!req.params.id.trim()){
+            throw new Error("error inesperado");
+        }
+        let query = 'SELECT * FROM categoria';
+        let respuesta = await qy(query, [req.params]);
+        res.status(200).send({respuesta});
+
+    }
+    catch (e) {
+        console.error(e.message);
+        res.status(403).send({ 'Error inesperado': e.message });
+    }
+})
+
+/*
+----------------------
+|MOSTRAR CATEGORIA/ID|
+----------------------
+*/
+
+app.get('/categoria/:id', async (req, res) => {
+    try {
+        if(!req.params.id.trim()){
+            throw new Error("error inesperado");
+        }
+        const query = 'SELECT * FROM categoria WHERE id = ?';
+        const respuesta = await qy(query, [req.params.id]);
+        if (respuesta.length == 0) {
+            throw new Error("la categoria no existe")
+        }
+        res.send({ respuesta })
+    }
+    catch (e) {
+        console.error(e.message);
+        res.status(403).send({ "Error": e.message })
+    }
+})
+
+
+/*
+---------------------
+|BORRAR CATEGORIA/ID|
+---------------------
+*/
+
+
+app.delete('/categoria/:id', async (req, res) => {
+    try {
+        //Validacion de categoria existente
+        let query = 'SELECT * FROM categoria WHERE id = ?'
+        respuesta = await qy(query, [req.params.id]);
+        if (respuesta.length == 0) {
+            throw new Error('No existe la categoria indicada');
+        }
+
+        //validacion de libros asociados
+        query = 'SELECT * FROM libro WHERE 	categoria_id=?'
+        respuesta = await qy(query, [req.params.id]);
+        console.log(respuesta);
+        if (respuesta.length != 0) {
+            throw new Error("Categoria con libros asociados, no se puede eliminar")
+        }
+        
+        query = 'DELETE FROM categoria WHERE id = ?';
+        respuesta = await qy(query, [req.params.id])
+        res.status(200).send({ mensaje:'Se borro correctamente' })
+    }
+    catch (e) {
+        console.error(e.message);
+        res.status(413).send({"mensaje": e.message });
+    }
+});
+
+// LIBRO
+
+/*
+    ------------------
+    |INGRESO DE LIBRO|
+    ------------------
+*/
+
 app.post('/libro', async (req, res) =>{
 try { 
     if(!req.body.nombre.trim() || !req.body.categoria_id.trim()){
@@ -225,7 +351,11 @@ try {
     
     }
 });
-
+/*
+---------------
+|MOSTRAR LIBRO|
+---------------
+*/
 
 app.get('/libro', async (req, res) =>{
     try {
@@ -243,7 +373,11 @@ app.get('/libro', async (req, res) =>{
 
 });
 
-
+/*
+------------------
+|MOSTRAR LIBRO/ID|
+------------------
+*/
 app.get('/libro/:id', async (req, res) =>{
     try {
         let query = 'SELECT * FROM libro  WHERE id = ?' ;
@@ -259,6 +393,12 @@ app.get('/libro/:id', async (req, res) =>{
     }
 
 });
+/*
+--------------------
+|ACTUALIZAR LIBRO/ID|
+--------------------
+*/
+
 app.put('/libro/:id', async(req, res) =>{
     try { 
         
@@ -286,6 +426,11 @@ app.put('/libro/:id', async(req, res) =>{
         res.status(413).send({"mensaje": e.message});
     }
     });
+/*
+--------------------
+|PRESTAR LIBRO/ID|
+--------------------
+*/
 
 app.put('/libro/prestar/:id', async (req, res) =>{
         try {
@@ -321,6 +466,11 @@ app.put('/libro/prestar/:id', async (req, res) =>{
             }
         
         });
+/*
+--------------------
+|DEVOLVER LIBRO/ID|
+--------------------
+*/
 
 app.put('/libro/devolver/:id', async (req, res) =>{
         try {
@@ -350,7 +500,11 @@ app.put('/libro/devolver/:id', async (req, res) =>{
             res.status(413).send({"mensaje": e.message});
             }
     });
-
+/*
+-----------------
+|BORRAR LIBRO/ID|
+-----------------
+*/
 app.delete('/libro/:id', async (req, res) =>{
                 try {
                     if(!req.params.id.trim()){
